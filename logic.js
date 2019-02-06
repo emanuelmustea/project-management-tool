8b927309201daf557ce6fb28f5b3d98fb09b3b3c
Emanuel Mustea Laptop <plus.emanuelmustea@gmail.com>
2019-02-06 19:30:32 +0200

    added creation methods  on each class

--- logic.js
+++ logic.js
@@ -6,7 +6,12 @@ var status = [
   "Resolved",
   "Ready for Testing"
 ];
-// function that generates an unique id
+var users = []; // an array of all users
+var sprints = []; // an array containing all sprints
+var issues = []; // an array containing all issues
+var tasks = []; // an array containing all issues
+var comments = []; // an array containing all issues
+// function that generates an unique id per application
 ID = () => {
   if (typeof ID.id == "undefined") ID.id = 1;
   else ID.id++;
@@ -24,6 +29,9 @@ class User {
     this.id = ID();
     this.name = name;
   }
+  get ID() {
+    return this.id;
+  }
 }
 /* Issue class
  * consturctor arguments: 
@@ -37,29 +45,74 @@ class User {
 class Issue {
   constructor(type, name, sprint, createdBy, assignee, description) {
     this.id = ID();
-    this.type = toUpperCase(type);
+    this.type = type.toUpperCase();
     this.name = name;
     this.sprint = sprint;
     this.createdBy = createdBy;
     this.assignee = assignee;
     this.description = description;
     this.status = 0; //initialize the status as New
-    this.createdBy = this.createdAt = new Date();
+    this.createdBy = createdBy;
+    this.createdAt = new Date();
     this.updatedAt = null; // null for never updated
-    //tasks will contain a list of task ids
-    if (type == "BUG" || type == "FEATURE") this.tasks = [];
+    this.updateCount = 0; // how many time was the issue updated
     //comments will contain a list of comment ids
     this.comments = [];
+    //tasks will contain a list of task ids
+    if (type == "BUG" || type == "FEATURE") this.tasks = [];
   }
+  get ID() {
+    return this.id;
+  }
+  update(newStatus = this.status) {
+    //change updatedAt and updateCount
+    this.updatedAt = new Date();
+    this.updateCount++;
+    //change the status in case of change
+    this.status = newStatus;
+  }
+  createSubTask(name, description) {
+    if (this.type == "TASK") return; //TASKS can't contain subtasks
+    //create new Issue but with SUBTASK type
+    let task = new Issue(
+      "SUBTASK",
+      name,
+      this.sprint,
+      this.createdBy,
+      this.assignee,
+      description
+    );
+    //add subtask id to subtasks list of Issue
+    this.tasks.push(task.ID);
+    //add sprint to global sprints variable
+    tasks.push(task);
+    //update the Issue date
+    this.update();
+  }
+  createComment() {}
 }
 /* Project class
  * constructor arguments:null
  */
 
 class Project {
-  consturctor() {
+  constructor() {
     this.id = ID();
     this.sprints = []; // a list of sprint ids
+  }
+  get ID() {
+    return this.id;
+  }
+  //creates Sprint and add it's id to sprints list
+  createSprint(name) {
+    // create a new Sprint object
+    let sprint = new Sprint(name);
+    //add sprint id to sprints list of Project
+    this.sprints.push(sprint.ID);
+    //add sprint to global sprints variable
+    sprints.push(sprint);
+
+    return sprint.ID;
   }
 }
 /* Sprint class
@@ -71,6 +124,25 @@ class Sprint {
     this.id = ID();
     this.name = name;
   }
+  get ID() {
+    return this.id;
+  }
+  //creates Issue and add it to global Issues variable
+  createIssue(type, name, createdBy, assignee, description) {
+    //create a new Issue object
+    let issue = new Issue(
+      type,
+      name,
+      this.id,
+      createdBy,
+      assignee,
+      description
+    );
+    //add issue to global issues variable
+    issues.push(issue);
+
+    return issue.ID;
+  }
 }
 /* Comment class
  * constructor arguments: 
@@ -81,4 +153,13 @@ class Comment {
     this.id = ID();
     this.name = name;
   }
+  get ID() {
+    return this.id;
+  }
 }
+var project = new Project();
+project.createSprint("mama");
+sprints[0].createIssue("BUG", "Mama mia", 1, 1, "Description of this issue");
+console.log(sprints);
+console.log(project);
+console.log(issues);
